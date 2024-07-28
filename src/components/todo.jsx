fa4b371d32b5d3e0fd3a6f8548240194a4d2f083
MandlakheM <142303272+MandlakheM@users.noreply.github.com>
2024-07-28 20:49:00 +0200

    fixed the time function

--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -5,41 +5,73 @@ import { LuListTodo } from "react-icons/lu";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { CiBurger } from "react-icons/ci";
 import { MdAdd } from "react-icons/md";
+import { ImCross } from "react-icons/im";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import TextField from "@mui/material/TextField";
-import DateRangeCalendarCalendarsProp from "./materialUI/calender";
-import ResponsiveTimePickers from "./materialUI/timePicker";
 import RowRadioButtonsGroup from "./materialUI/radioGroup";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
-// import Button from "@mui/material/Button";
-import { ImCross } from "react-icons/im";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import dayjs from "dayjs";
+
 
 function Todo() {
   const [modal, setModal] = useState(false);
-  const [task, setTask] = useState([]);
-  const [newTask, setNewTask] = useState("");
-
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState({
+    taskName: "",
+    taskTime: dayjs().format("HH:mm"),
+    taskDate: dayjs(),
+    importance: "",
+  });
   const [open, setOpen] = React.useState(false);
 
   const toggleDrawer = (newOpen) => () => {
     setOpen(newOpen);
   };
 
-  const DrawerList = (
-    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
-      <AccountCircleIcon sx={{ fontSize: 100}} />
-    </Box>
-  );
-
   function handleChange(event) {
-    setNewTask(event.target.value);
+    const { name, value } = event.target;
+    setNewTask((prevTask) => ({
+      ...prevTask,
+      [name]: value,
+    }));
+  }
+
+  function handleDateChange(newValue) {
+    setNewTask((prevTask) => ({
+      ...prevTask,
+      taskDate: newValue,
+    }));
+  }
+
+  function handleImportanceChange(event) {
+    const { value } = event.target;
+    setNewTask((prevTask) => ({
+      ...prevTask,
+      importance: value,
+    }));
   }
 
   function addTasks() {
-    setTask((t) => [...t, newTask]);
-    setNewTask("");
+    if (newTask.taskName.trim() !== "") {
+      setTasks((prevTasks) => [...prevTasks, newTask]);
+      setNewTask({
+        taskName: "",
+        taskTime: dayjs().format("HH:mm"),
+        taskDate: dayjs(),
+        importance: "",
+      });
+    }
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    addTasks();
+    setModal(false);
   }
 
   const activateModal = () => {
@@ -48,6 +80,25 @@ function Todo() {
 
   const deactivateModal = () => {
     setModal(false);
+  };
+
+  const DrawerList = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+      <AccountCircleIcon sx={{ fontSize: 100 }} />
+    </Box>
+  );
+
+  const getBorderColor = (importance) => {
+    switch (importance) {
+      case "very":
+        return "red";
+      case "slightly":
+        return "yellow";
+      case "average":
+        return "green";
+      default:
+        return "transparent";
+    }
   };
 
   return (
@@ -62,7 +113,7 @@ function Todo() {
           </div>
           <div className="dateContainer">
             <div className="date">
-              <p>2024-07-23</p>
+              <p>{dayjs().format("YYYY-MM-DD")}</p>
             </div>
             <div className="search">
               <input type="text" />
@@ -82,42 +133,29 @@ function Todo() {
         </div>
 
         <div className="tasks">
-            <div className="taskCard">
+          {tasks.map((task, index) => (
+            <div className="taskCard" key={index}
+            style={{ border: `2px solid ${getBorderColor(task.importance)}` }}
+            >
               <div className="taskIcon">
                 <CiBurger id="headerIcons" />
               </div>
               <div className="time">
-                <p id="taskTime">08:00am - 09:00 am</p>
-                <p>Take the kids to school</p>
+                <p id="taskTime">Task created on: {task.taskTime}</p>
+                <p>{task.taskName}</p>
               </div>
               <div className="taskDate">
-                <p>
-                  Thursday <br />
-                  25
+              <p>
+                  {task.taskDate ? task.taskDate.format("dddd MMM DD") : ""} 
+                  {task.importance}
                 </p>
               </div>
             </div>
-
-
-          <div className="taskCard">
-            <div className="taskIcon">
-              <CiBurger id="headerIcons" />
-            </div>
-            <div className="time">
-              <p id="taskTime">08:00am - 09:00 am</p>
-              <p>Take the kids to school</p>
-            </div>
-            <div className="taskDate">
-              <p>
-                Thursday <br />
-                25
-              </p>
-            </div>
-          </div>
+          ))}
         </div>
 
         <div className="addButton">
-          <button onClick={() => activateModal()}>
+          <button onClick={activateModal}>
             <MdAdd />
           </button>
         </div>
@@ -127,19 +165,33 @@ function Todo() {
             <div className="overlay" onClick={deactivateModal}></div>
             <div className="modalContent">
               <Box component="section" sx={{ p: 2, border: "1px dashed grey" }}>
-                <DateRangeCalendarCalendarsProp />
-                {/* <input type="datetime-local" name="" id="" /> */}
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <DateCalendar
+                    disablePast
+                    name="taskDate"
+                    value={newTask.taskDate}
+                    onChange={handleDateChange}
+                  />
+                </LocalizationProvider>
                 <TextField
                   id="filled-basic"
                   label="Task name"
                   variant="filled"
+                  name="taskName"
+                  value={newTask.taskName}
+                  onChange={handleChange}
                 />
                 <br />
-                <ResponsiveTimePickers />
                 <br />
-                <RowRadioButtonsGroup />
+                <RowRadioButtonsGroup
+                  name="importance"
+                  value={newTask.importance}
+                  onChange={handleImportanceChange}
+                />
                 <br />
-                <Button variant="outlined">ADD TASK</Button>
+                <Button variant="outlined" onClick={handleSubmit}>
+                  ADD TASK
+                </Button>
               </Box>
               <div className="closeModal">
                 <ImCross onClick={deactivateModal} />
